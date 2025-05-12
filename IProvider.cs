bf72059cdb711ad504d827c07c5a5ab3a799b4dd
paul <paul@MAINPC>
2025-05-12 05:17:03 +0300

    Specify spectra by numbers instead of indexes

--- IProvider.cs
+++ IProvider.cs
@@ -5,4 +5,13 @@ public interface IProvider
     public string Comment { get; }
     public List<Spectrum> Spectra { get; }
     public int TotalSpectra => Spectra.Count;
+    /// <summary>
+    /// Provides spectrum number-based access, starts with 1
+    /// </summary>
+    /// <param name="index"></param>
+    /// <returns></returns>
+    public Spectrum this[int index]
+    {
+        get => Spectra[index + 1];
+    } 
 }
